b8760723aa014fe3df10beeb5f77f0b5b46a4d51
Dhruv1306 <dhruvdalal122@gmail.com>
2025-05-16 08:06:29 +0530

    Update Vite config to use docs/ as root and clean up project structure

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 export default {
-  // Set the base directory for GitHub pages
-  base: '/simcity-threejs-clone/',
+  // Set the base directory for GitHub pages (update if your repo name is different)
+  base: '/EcoCity_A_Green_Urban_WebGame/',
 
-  // Set the project root directory (relative to the config file)
-  root: './src',
+  // Set the project root directory to docs
+  root: './docs',
 
   // Set the directory to serve static files from (relative to the root)
-  publicDir: './public',
-  
+  publicDir: './public', // Only if you have a docs/public folder with extra static assets. If not, you can remove this line.
+
   // Set the build output directory
   build: {
-    outDir: './dist'
+    outDir: '../dist' // This will put the build output outside docs, which is common
   }
 }
